97ef9560c2fabf1e6b40e15c33126790ec2f54dd
saradir <120840462+saradir@users.noreply.github.com>
2025-12-17 23:58:16 +0200

    update post model in prisma

--- api/src/routers/postRouter.ts
+++ api/src/routers/postRouter.ts
@@ -3,10 +3,12 @@ import * as postController from "../controllers/postController";
 import { commentRouter } from "./commentRouter";
 export const postRouter = express.Router();
 import { jwtAuth } from "../middleware/jwtAuth";
+import * as authorize from "../middleware/authorize"
+
 
 postRouter.get("/", postController.index);
 postRouter.get("/:postId", postController.show );
-postRouter.post("/", jwtAuth, postController.create);
-postRouter.put("/:postId", postController.edit);
-postRouter.delete("/:postId", postController.remove);
+postRouter.post("/", jwtAuth, authorize.requireAdmin, postController.create);
+postRouter.put("/:postId",jwtAuth, authorize.requireAdmin, postController.edit);
+postRouter.delete("/:postId", jwtAuth, authorize.requireAdmin, postController.remove);
 postRouter.use("/:postId/comments", commentRouter);
