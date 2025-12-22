2000e3581e922385605b2b6b473d0505eac5f090
saradir <120840462+saradir@users.noreply.github.com>
2025-12-22 22:49:59 +0200

    add some css to post page

--- public-client/src/components/PostPage.jsx
+++ public-client/src/components/PostPage.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import CommentList from "./CommentList";
+import "../styles/PostPage.css";
+
 function PostPage() {
 
     const { postId } = useParams();
