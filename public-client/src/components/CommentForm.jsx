abd0934b11281ac1e6c821e67d07b6e5b13d81fb
saradir <120840462+saradir@users.noreply.github.com>
2025-12-24 21:56:23 +0200

    refactor comment form to include show comments logic

--- public-client/src/components/CommentForm.jsx
+++ public-client/src/components/CommentForm.jsx
@@ -1,16 +1,27 @@
-function CommentForm({handleSubmit, handleChange, text}){
+import { useState } from "react";
 
-return(
-    <form className="comment-form" onSubmit={handleSubmit}>
-        <h4 className="comment-form-title">Write a comment</h4>
-        <textarea
-        value={text}
-        onChange={handleChange}
-        />
 
-        <button type="submit" disabled={text.trim() === ''}>Add comment</button>
-    </form>
-);
+function CommentForm(){
+    const [commentText, setCommentText] = useState('');
+    function handleChange(e){
+    setCommentText(e.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(commentText);
+    }
+    return(
+        <form className="comment-form" onSubmit={handleSubmit}>
+            <h4 className="comment-form-title">Write a comment</h4>
+            <textarea
+            value={commentText}
+            onChange={handleChange}
+            />
+
+            <button type="submit" disabled={commentText.trim() === ''}>Add comment</button>
+        </form>
+    );
 }
 
 export default CommentForm;
--- public-client/src/components/Post.jsx
+++ public-client/src/components/Post.jsx
@@ -10,27 +10,17 @@ import CommentForm from "./CommentForm";
 export function Post({post}){
     const [showComments, setShowComments] = useState(false);
     const [ showForm, setShowForm] = useState(false);
-    const [commentText, setCommentText] = useState('');
     const toggleCommentView = () => setShowComments(v => !v);
     const toggleCommentForm = () => setShowForm(v => !v);
 
 
-    function handleChange(e){
-        setCommentText(e.target.value);
-    }
-
-    function handleSubmit(e){
-        e.preventDefault();
-        console.log(commentText);
-    }
-    console.log(post);
     return(
         <div className="post">
             <PostLayout post={post}>               
                 <DiscussionControls commentCount={post._count.comments} toggleCommentView={toggleCommentView} toggleCommentForm={toggleCommentForm} showComments={showComments} />
             </PostLayout>
 
-            {showForm && <CommentForm handleChange={handleChange} handleSubmit={handleSubmit} text={commentText} />}
+            {showForm && <CommentForm   />}
             {showComments && <CommentList postId={post.id} />}
 
             
