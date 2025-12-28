03a4c961c322d2c31c3a7b4553f17a467400085b
saradir <120840462+saradir@users.noreply.github.com>
2025-12-28 17:23:27 +0200

    add basic css to all components

--- admin-client/src/components/Post.jsx
+++ admin-client/src/components/Post.jsx
@@ -1,7 +1,17 @@
+import "../styles/Post.css"
+
 function Post({post}){
+
+    const status = post.isDraft? "draft" : "published";
     return(
         <div className="post">
+
             <span className="post-title">{post.title}</span>
+            {post.publishedAt && <span className="publish-date">{new Date(post.publishedAt).toLocaleDateString()}</span>}
+            <div className="post-actions">
+                <button>Edit</button>
+                <button className={`status ${status}`}>{status}</button>
+            </div>
         </div>
     )
 }
